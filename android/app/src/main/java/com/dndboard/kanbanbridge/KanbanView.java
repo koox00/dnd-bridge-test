110e45ae5177b3e9b66fd8e4cdab59a44f0587f5
Kyriakos Ziakoulis <cooxlee@gmail.com>
2018-04-03 13:52:15 +0300

    fix on layout
    - dragging a card now reflects the changes in the ui

--- android/app/src/main/java/com/dndboard/kanbanbridge/KanbanView.java
+++ android/app/src/main/java/com/dndboard/kanbanbridge/KanbanView.java
@@ -72,5 +72,20 @@ public class KanbanView extends FrameLayout {
         super.onDetachedFromWindow();
     }
 
+    @Override
+    public void requestLayout() {
+        super.requestLayout();
+        post(measureAndLayout);
+    }
+
+    private final Runnable measureAndLayout = new Runnable() {
+        @Override
+        public void run() {
+            measure(
+                    MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.EXACTLY),
+                    MeasureSpec.makeMeasureSpec(getHeight(), MeasureSpec.EXACTLY));
+            layout(getLeft(), getTop(), getRight(), getBottom());
+        }
+    };
 
 }
